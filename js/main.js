04de3d0ca561536a44aebeda6d24d8fb18a55408
RauPro <00012119@uca.edu.sv>
2021-11-16 19:01:36 -0600

    Add: open query

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 const username = "RauPro"
-const ioKey = "aio_DCqp676VR0NF79PF3xiNAXfatlbx"
+const ioKey = "aio_zLuf36ARk28MViso2h0eVtPAF8ex"
 const apiUrl = "https://io.adafruit.com";
+const storage = !localStorage.getItem('door') ? [] : JSON.parse(localStorage.getItem('door'));
 
+const createTable = () => {
+    document.querySelector('.table-door').innerHTML = `
+            <thead>
+            <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Status</th>
+            </tr>
+            </thead>
+           <tbody>
+    `;
+    for (let data of storage){
+        document.querySelector('.table-door').innerHTML+= `
+            <tr>
+                <td>${data.date}</td>
+                <td>${data.status}</td>
+            </tr>
+        `;
+    }
+    document.querySelector('.table-door').innerHTML+= ` <tbody>`
+
+}
+createTable();
 async function getFeeds(url = '') {
     const response = await fetch(url, {
         method: 'GET',
@@ -11,4 +34,58 @@ async function getFeeds(url = '') {
     });
     return response.json();
 }
+
+async function getFeed(key) {
+    const response = await fetch(apiUrl + `/api/v2/${username}/feeds/${key}`, {
+        method: 'GET',
+        headers: {
+            'X-AIO-Key': `${ioKey}`
+        },
+    });
+    return response.json();
+}
+
+async function updateFeed(feed_key, body) {
+    const response = await fetch(apiUrl + `/api/v2/${username}/feeds/${feed_key}/data`, {
+        method: 'POST',
+        headers: {
+            'X-AIO-Key': `${ioKey}`,
+        },
+        body,
+
+
+    });
+    return response.json();
+}
+
 getFeeds(apiUrl + `/api/v2/${username}/feeds/`).then(data => console.log(data))
+getFeed('door').then(data => console.log(data))
+
+const doorBtn = document.querySelector('.door-button');
+const doorInput = document.querySelector('.key-door');
+
+doorBtn.addEventListener("click", ()=>{
+    if (doorInput.value==="1234"){
+        const body = new FormData
+        body.append("value", "ON")
+        updateFeed('door', body).then(data => console.log(data))
+        body.delete('value');
+        body.append("value", "OFF")
+        setInterval(() => {
+            updateFeed('door', body).then(data => console.log(data))
+            const current = {date: new Date().toString().substring(0, 21), status: 'Open'}
+            storage.unshift(current);
+            localStorage.setItem('door', JSON.stringify(storage))
+            createTable();
+            location.reload();
+        }, 1000)
+
+    }
+    else {
+        alert("Invalid key")
+    }
+})
+
+
+
+
